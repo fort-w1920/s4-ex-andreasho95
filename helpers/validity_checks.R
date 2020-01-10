1c3e872f7f9f319b27a718198eca2ae879a8331a
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2020-01-11 00:30:53 +0100

    Improved check_numeric_slot

--- helpers/validity_checks.R
+++ helpers/validity_checks.R
@@ -1,7 +1,18 @@
 # Checks validity of numeric slotsS
-# Returns TRUE if invalid
-check_numeric_slot <- function(slot, lower = 0, upper = 1e5) {
-  is.na(slot) || length(slot) > 1 || slot < lower || slot > upper
+# Returns NULL if valid
+# Returns message for specific slot when invalid
+check_numeric_slot <- function(slot, lower = -Inf, upper = Inf) {
+  invalid <- is.na(slot) || length(slot) > 1 || slot < lower || slot > upper
+  if (invalid) {
+    char_slot <- deparse(substitute(slot))
+    slot_str <- stringr::str_extract(char_slot, "(?<=@).*")
+    msg <- paste0("@", slot_str, " must be a numeric of length 1")
+    if (lower != -Inf & upper != Inf) {
+      msg <- paste0(msg, " in [", lower, ", ", upper, "]")
+    }
+    return(msg)
+  }
+  NULL
 }
 
 
@@ -9,16 +20,17 @@ setValidity("animal", function(object) {
   invalids <- character(0)
   invalid_name <- is.na(object@name) || length(object@name) > 1 || nchar(object@name) == 0
   invalid_female <- is.na(object@female) || length(object@female) > 1
-  invalid_weight <- check_numeric_slot(object@weight)
+
   if (invalid_name) {
     invalids <- "@name must be a character of length 1"
   }
   if (invalid_female) {
     invalids <- c(invalids, "@female must be a logical of length 1")
   }
-  if (invalid_weight) {
-    invalids <- c(invalids, "@weight must be a numeric of length 1")
-  }
+
+  invalids <- c(invalids, check_numeric_slot(object@weight))
+  
+  
   # Early return in case of invalids
   if (length(invalids) > 0) {
     return(invalids)
@@ -28,10 +40,8 @@ setValidity("animal", function(object) {
 
 setValidity("prey", function(object) {
   invalids <- character(0)
-  invalid_hide <- check_numeric_slot(object@hide, 0, 1)
-  if (invalid_hide) {
-    invalids <- "@hide must be a numeric of length 1 in [0, 1]"
-  }
+  invalids <- c(invalids, check_numeric_slot(object@hide, 0, 1))
+
   # Early return in case of invalids
   if (length(invalids) > 0) {
     return(invalids)
@@ -41,10 +51,8 @@ setValidity("prey", function(object) {
 
 setValidity("predator", function(object) {
   invalids <- character(0)
-  invalid_seek <- check_numeric_slot(object@seek, 0, 1)
-  if (invalid_seek) {
-    invalids <- "@seek must be a numeric of length 1 in [0, 1]"
-  }
+  invalids <- c(invalids, check_numeric_slot(object@seek, 0, 1))
+  
   # Early return in case of invalids
   if (length(invalids) > 0) {
     return(invalids)
@@ -54,14 +62,9 @@ setValidity("predator", function(object) {
 
 setValidity("mouse", function(object) {
   invalids <- character(0)
-  invalid_weight <-  check_numeric_slot(object@weight, 0.5, 1)
-  invalid_hide <- check_numeric_slot(object@hide, 0.6, 1)
-  if (invalid_weight) {
-    invalids <- "@weight must be a numeric of length 1 in [0.5, 1]"
-  }
-  if (invalid_hide) {
-    invalids <- c(invalids, "@hide must be a numeric of length 1 in [0.6, 1]")
-  }
+  invalids <- c(invalids, check_numeric_slot(object@weight, 0.5, 1))
+  invalids <- c(invalids, check_numeric_slot(object@hide, 0.6, 1))
+  
   # Early return in case of invalids
   if (length(invalids) > 0) {
     return(invalids)
@@ -71,14 +74,9 @@ setValidity("mouse", function(object) {
 
 setValidity("rabbit", function(object) {
   invalids <- character(0)
-  invalid_weight <- check_numeric_slot(object@weight, 1, 5)
-  invalid_hide <- check_numeric_slot(object@weight, 0.3, 0.8)
-  if (invalid_weight) {
-    invalids <- "@weight must be a numeric of length 1 in [1, 5]"
-  }
-  if (invalid_hide) {
-    invalids <- c(invalids, "@hide must be a numeric of length 1 in [0.3, 0.8]")
-  }
+  invalids <- c(invalids, check_numeric_slot(object@weight, 1, 5))
+  invalids <- c(invalids, check_numeric_slot(object@hide, 0.3, 0.8))
+  
   # Early return in case of invalids
   if (length(invalids) > 0) {
     return(invalids)
@@ -88,14 +86,9 @@ setValidity("rabbit", function(object) {
 
 setValidity("deer", function(object) {
   invalids <- character(0)
-  invalid_weight <- check_numeric_slot(object@weight, 15, 30)
-  invalid_hide <- check_numeric_slot(object@weight, 0.2, 0.7)
-  if (invalid_weight) {
-    invalids <- "@weight must be a numeric of length 1 in [15, 30]"
-  }
-  if (invalid_hide) {
-    invalids <- c(invalids, "@hide must be a numeric of length 1 in [0.2, 0.7]")
-  }
+  invalids <- c(invalids, check_numeric_slot(object@weight, 15, 30))
+  invalids <- c(invalids, check_numeric_slot(object@hide, 0.2, 0.7))
+  
   # Early return in case of invalids
   if (length(invalids) > 0) {
     return(invalids)
@@ -105,14 +98,9 @@ setValidity("deer", function(object) {
 
 setValidity("hawk", function(object) {
   invalids <- character(0)
-  invalid_weight <- check_numeric_slot(object@weight, 3, 8)
-  invalid_seek <- check_numeric_slot(object@seek, 0.6, 1)
-  if (invalid_weight) {
-    invalids <- "@weight must be a numeric of length 1 in [3, 8]"
-  }
-  if (invalid_seek) {
-    invalids <- c(invalids, "@seek must be a numeric of length 1 in [0.6, 1]")
-  }
+  invalids <- c(invalids, check_numeric_slot(object@weight, 3, 8))
+  invalids <- c(invalids, check_numeric_slot(object@seek, 0.6, 1))
+  
   # Early return in case of invalids
   if (length(invalids) > 0) {
     return(invalids)
@@ -122,14 +110,9 @@ setValidity("hawk", function(object) {
 
 setValidity("lynx", function(object) {
   invalids <- character(0)
-  invalid_weight <- check_numeric_slot(object@weight, 20, 60)
-  invalid_seek <- check_numeric_slot(object@seek, 0.5, 0.9)
-  if (invalid_weight) {
-    invalids <- "@weight must be a numeric of length 1 in [20, 60]"
-  }
-  if (invalid_seek) {
-    invalids <- c(invalids, "@seek must be a numeric of length 1 in [0.5, 0.9]")
-  }
+  invalids <- c(invalids, check_numeric_slot(object@weight, 20, 60))
+  invalids <- c(invalids, check_numeric_slot(object@seek, 0.5, 0.9))
+  
   # Early return in case of invalids
   if (length(invalids) > 0) {
     return(invalids)
