38747b525b7183ce2df0babedb3a858df7d77104
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2020-01-13 17:25:32 +0100

    Added tests for correct cases

--- helpers/tests.R
+++ helpers/tests.R
@@ -89,4 +89,74 @@ test_that("lynx class handles incorrect input correctly", {
 })
 
 
+context("Classes handle correctly with proper input")
+test_that("animal class handles proper input correctly", {
+  test_animal <- animal(name = "Dieter", weight = 20, female = FALSE)
+  expect_true(class(test_animal) == "animal")
+  expect_true(test_animal@name == "Dieter")
+  expect_true(test_animal@female == FALSE)
+  expect_true(test_animal@weight == 20)
+})
 
+test_that("prey class handles proper input correctly", {
+  test_prey <- prey(name = "Dieter", weight = 20, female = FALSE, hide = 1)
+  expect_true(class(test_prey) == "prey")
+  expect_true(test_prey@name == "Dieter")
+  expect_true(test_prey@female == FALSE)
+  expect_true(test_prey@weight == 20)
+  expect_true(test_prey@hide == 1)
+})
+
+test_that("predator class handles proper input correctly", {
+  test_predator <- predator(name = "Dieter", weight = 20, female = FALSE, seek = 0.5)
+  expect_true(class(test_predator) == "predator")
+  expect_true(test_predator@name == "Dieter")
+  expect_true(test_predator@female == FALSE)
+  expect_true(test_predator@weight == 20)
+  expect_true(test_predator@seek == 0.5)
+})
+
+test_that("mouse class handles proper input correctly", {
+  test_mouse <- mouse(name = "Dieter", weight = 0.5, female = TRUE, hide = 0.8)
+  expect_true(class(test_mouse) == "mouse")
+  expect_true(test_mouse@name == "Dieter")
+  expect_true(test_mouse@female == TRUE)
+  expect_true(test_mouse@weight == 0.5)
+  expect_true(test_mouse@hide == 0.8)
+})
+
+test_that("rabbit class handles proper input correctly", {
+  test_rabbit <- rabbit(name = "Dieter", weight = 2, female = TRUE, hide = 0.8)
+  expect_true(class(test_rabbit) == "rabbit")
+  expect_true(test_rabbit@name == "Dieter")
+  expect_true(test_rabbit@female == TRUE)
+  expect_true(test_rabbit@weight == 2)
+  expect_true(test_rabbit@hide == 0.8)
+})
+
+test_that("deer class handles proper input correctly", {
+  test_deer <- deer(name = "Dieter", weight = 15, female = TRUE, hide = 0.4)
+  expect_true(class(test_deer) == "deer")
+  expect_true(test_deer@name == "Dieter")
+  expect_true(test_deer@female == TRUE)
+  expect_true(test_deer@weight == 15)
+  expect_true(test_deer@hide == 0.4)
+})
+
+test_that("hawk class handles proper input correctly", {
+  test_hawk <- hawk(name = "Dieter", weight = 7, female = TRUE, seek = 0.8)
+  expect_true(class(test_hawk) == "hawk")
+  expect_true(test_hawk@name == "Dieter")
+  expect_true(test_hawk@female == TRUE)
+  expect_true(test_hawk@weight == 7)
+  expect_true(test_hawk@seek == 0.8)
+})
+
+test_that("lynx class handles proper input correctly", {
+  test_lynx <- lynx(name = "Dieter", weight = 37, female = TRUE, seek = 0.8)
+  expect_true(class(test_lynx) == "lynx")
+  expect_true(test_lynx@name == "Dieter")
+  expect_true(test_lynx@female == TRUE)
+  expect_true(test_lynx@weight == 37)
+  expect_true(test_lynx@seek == 0.8)
+})
